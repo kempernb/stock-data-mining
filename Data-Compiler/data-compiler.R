e2904a063dee9d1b1d82238fbb92811937b36014
Nicholas Kemper <kempernb@mail.uc.edu>
2021-04-18 21:12:47 -0400

    Update data-compiler.R

--- Data-Compiler/data-compiler.R
+++ Data-Compiler/data-compiler.R
@@ -1,12 +1,11 @@
 ### Simple input of data
 library(tidyverse)
 library(tidyquant)
-library(quantmod)
 
 getLargeStocks <- function() {
   # Get and read all sector data ----
 
-  # Get SP500 data
+  # Get SP500 data only for now
   stock_index <-tq_index("SP500")
   stock_index <- drop_na(stock_index)
 
@@ -15,6 +14,7 @@ getLargeStocks <- function() {
   for (i in stock_index$symbol) {
     print(i)
     
+    # Get the stock prices! Includes error handeling if occurs
     prices <- tryCatch({
       tq_get(i, get = "stock.prices")
     }, error = function(e) {
@@ -25,15 +25,15 @@ getLargeStocks <- function() {
       next
     }
     prices$date <- as.Date(prices$date)
-    if (nrow(prices) < 400) {
+    if (nrow(prices) < 200) {
       print("Not enough data")
       next
     }
     
     data <- rbind(data, prices)
   }
-  # Write Final Data
   
+  # Write Final Data
   write_rds(data, "dailyPrice.RDS")
 }
 
